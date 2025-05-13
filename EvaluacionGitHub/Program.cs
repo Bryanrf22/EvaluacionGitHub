c7e5f3d11fc413ef94ac028b70d9c96fa3f97cbc
Bryan Romero <bryanromerof26@gmail.com>
2025-05-13 13:42:30 -0600

    Primer commit del Proyecto

---
+++ EvaluacionGitHub/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EvaluacionGitHub
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Proyecto de Ventas");
+        }
+    }
+}
