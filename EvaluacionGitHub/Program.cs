25b64e7967e7259578543e746dd8e3293b9a02dc
Bryan Romero <bryanromerof26@gmail.com>
2025-05-13 13:59:38 -0600

    Añadido metodo que muestra mi información

--- EvaluacionGitHub/Program.cs
+++ EvaluacionGitHub/Program.cs
@@ -4,7 +4,14 @@
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Proyecto de Ventas");
+            Console.WriteLine("Proyecto de Ventas \n");
+
+            MostrarDatos();
+        }
+
+        public static void MostrarDatos()
+        {
+            Console.WriteLine("Mi nombre es: Bryan Enrique Romero Funes, y mi carnet es: 24510477");
         }
     }
 }
